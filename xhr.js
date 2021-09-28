517e3f84037fad2e4c54609c1a88e2bdf717dea3
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2021-09-28 19:11:48 +0530

    country

---
+++ xhr.js
@@ -0,0 +1,22 @@
+
+const getBlogs = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        console.log(xhr.response)
+        const so = xhr.response;
+        for (let blog of so) {
+            // console.log(blog)
+            console.log(blog.name, blog.region, blog.subregion, blog.population)
+
+
+        }
+
+    };
+
+    xhr.send();
+
+};
+getBlogs();
+
